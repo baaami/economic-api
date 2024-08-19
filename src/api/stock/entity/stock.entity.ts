74b04db187c61a735ffcfa6b5067f86740eb429a
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-08-19 14:16:05 +0900

    feat: stock_table 데이터 수정
    - 화면에 뿌려질 data schema로 변경

--- src/api/stock/entity/stock.entity.ts
+++ src/api/stock/entity/stock.entity.ts
@@ -1,11 +1,26 @@
 import { CommonEntity } from 'src/api/common/entity/common.entity';
 import { Context } from 'src/api/context/entity/context.entity';
-import { Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 
 @Entity({ schema: 'economic_context', name: 'stocks' })
 export class Stock extends CommonEntity {
   @PrimaryColumn({ type: 'varchar', name: 'stock_symbol', length: 20 })
   stockSymbol: string;
+
+  @Column({ type: 'number', name: 'stock_price' })
+  stockPrice: number;
+
+  @Column({ type: 'number', name: 'stock_offset' })
+  stockOffset: number;
+
+  @Column({ type: 'varchar', name: 'stock_description', length: 300 })
+  stockDescription: string;
+
+  @Column({ type: 'varchar', name: 'stock_CEO', length: 50 })
+  stockCEO: string;
+
+  @Column({ type: 'varchar', name: 'stock_founded', length: 50 })
+  stockFounded: string;
 
   @OneToMany(() => Context, (context) => context.stock)
   contexts: Context[];
